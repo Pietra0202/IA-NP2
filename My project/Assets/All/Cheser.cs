2ef33be79b2ffddcb10c76797dc5189ec2cc67b1
Pietra0202 <bernavapietra@gmail.com>
2024-11-22 20:43:50 -0300

    First Commit

---
+++ My project/Assets/All/Cheser.cs
@@ -0,0 +1,94 @@
+//using System;
+//using UnityEngine;
+//using UnityEngine.AI;
+
+//public class Stalker : MonoBehaviour
+//{
+// [SerializeField] private NavMeshAgent agent = null;
+// [SerializeField] private Transform target = null;
+
+// void Start()
+// {
+// if (agent == null)
+// {
+// if (!TryGetComponent(out agent))
+// {
+// Debug.LogWarning(name + "precisa colocar um navmesh agent");
+// }
+// }
+
+// }
+// void Update()
+// {
+// if (target)
+// {
+// MoveToTarget();
+// }
+// }
+
+// private void MoveToTarget()
+// {
+// agent.SetDestination(target.position);
+// agent.isStopped = false;
+// }
+//}
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Cheser : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent = null;
+    [SerializeField] private Transform target = null;
+
+    //Distância para ele eliminar o alvo
+    [SerializeField] private float killDistance = 2f;
+
+    //Quanto o npc tem que girar para atingir o alvo para eliminalo
+    [SerializeField] private float lookThreshold = .8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (agent == null)
+        {
+            if (!TryGetComponent(out agent))
+            {
+                Debug.LogWarning(name + "precisa colocar um navmesh agent");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            MoveToTarget();
+        }
+
+        if (Vector3.Dot(transform.forward.normalized, target.position.normalized) <= lookThreshold
+            && Vector3.Distance(transform.position, target.position) <= killDistance)
+
+            KillTarget();
+
+        return;
+    }
+
+    private void MoveToTarget()
+    {
+        agent.SetDestination(target.position);
+        agent.isStopped = false;
+    }
+
+    private void KillTarget()
+    {
+        Destroy(target.gameObject);
+        target = null;
+    }
+
+}
---
+++ My project/Assets/All/awayRun.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AwayRun : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent = null;
+    [SerializeField] private Transform cheser = null;
+    [SerializeField] private float travelCost = 5f;
+    private object m_Curve;
+
+    void Start()
+    {
+
+        if (agent == null)
+        {
+            if (!TryGetComponent(out agent))
+            {
+                Debug.LogWarning(name + "precisa colocar um navmesh agent");
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (cheser == null)
+            return;
+
+        // Calcula a direção normalizada para fugir do perseguidor
+        Vector3 directionNormalized = (cheser.position - transform.position).normalized;
+
+        // Adiciona um desvio aleatório à direção
+        MoveToPos(transform.position - (directionNormalized * travelCost));
+        
+        directionNormalized = Quaternion.AngleAxis(Random.Range(0, 179), Vector3.up) * directionNormalized;
+        
+    }
+
+    private void MoveToPos(Vector3 position)
+
+    {
+        agent.SetDestination(position);
+        agent.isStopped = false;
+    }
+
+  
+
+}
