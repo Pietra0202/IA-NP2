924736a8a97841b26e08363de203284432295fdd
Pietra0202 <bernavapietra@gmail.com>
2024-11-23 23:04:39 -0300

    NP2

---
+++ My project/Assets/All/Patrolling.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrolling : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed = 2f;
+    private int targetPoint = 0;
+
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+
+    void Update()
+    {
+
+        if (transform.position == waypoints[targetPoint].position)
+        {
+            targetPoint = Random.Range(0, waypoints.Length);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[targetPoint].position, speed * Time.deltaTime);
+    }
+}
